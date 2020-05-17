edea5937c3e47612a4bac23c5546b6d8b0c96617
Dmitriy <klopov@kontur.ru>
2020-05-17 12:40:49 +0500

    base api url with env

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,10 @@
 import axios from 'axios'
 
+const baseUrl = process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3000/api'
+    : process.env.REACT_APP_API_URL;
 const api = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL: baseUrl,
 });
 
 export const updateActivity = payload => api.post(`/activity`, payload);
